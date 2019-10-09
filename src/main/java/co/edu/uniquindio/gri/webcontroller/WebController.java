565c59fa7ed3062b784ade39e3b2b84817c823ba
Juan Alvarez <Pyrosea98@gmail.com>
2019-10-09 12:46:02 -0500

    Adición de totales a investigadores internos

--- src/main/java/co/edu/uniquindio/gri/webcontroller/WebController.java
+++ src/main/java/co/edu/uniquindio/gri/webcontroller/WebController.java
@@ -158,6 +158,9 @@ public class WebController {
 				} else if (subType.equals("fp")) {
 					model.addAttribute("listaInvestigadores",
 							investigadorDAO.getInvestigadoresInternosPregradoGrupo(Long.parseLong(id)));
+				} else if (subType.equals("d")) {
+					model.addAttribute("listaInvestigadores",
+							investigadorDAO.getAllInvestigadoresInternosGrupo(Long.parseLong(id)));
 				} else {
 					model.addAttribute("listaInvestigadores",
 							investigadorDAO.getInvestigadoresGrupo(Long.parseLong(id)));
@@ -190,6 +193,9 @@ public class WebController {
 				} else if (subType.equals("fp")) {
 					model.addAttribute("listaInvestigadores",
 							investigadorDAO.getInvestigadoresInternosPregradoCentro(Long.parseLong(id)));
+				} else if (subType.equals("d")) {
+					model.addAttribute("listaInvestigadores",
+							investigadorDAO.getAllInvestigadoresInternosCentro(Long.parseLong(id)));
 				} else {
 					model.addAttribute("listaInvestigadores",
 							investigadorDAO.getInvestigadoresCentro(Long.parseLong(id)));
@@ -222,6 +228,9 @@ public class WebController {
 				} else if (subType.equals("fp")) {
 					model.addAttribute("listaInvestigadores",
 							investigadorDAO.getInvestigadoresInternosPregradoPrograma(Long.parseLong(id)));
+				} else if (subType.equals("d")) {
+					model.addAttribute("listaInvestigadores",
+							investigadorDAO.getAllInvestigadoresInternosPrograma(Long.parseLong(id)));
 				} else {
 					model.addAttribute("listaInvestigadores",
 							investigadorDAO.getInvestigadoresPrograma(Long.parseLong(id)));
