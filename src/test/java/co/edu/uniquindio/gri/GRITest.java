056f4db1faef5573da0ecf6efd3b5a58a03fb24a
Juan Alvarez <Pyrosea98@gmail.com>
2019-11-05 09:24:20 -0500

    Test20

---
+++ src/test/java/co/edu/uniquindio/gri/GRITest.java
@@ -0,0 +1,40 @@
+package co.edu.uniquindio.gri;
+
+import org.junit.*;
+import org.junit.runner.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.test.autoconfigure.web.servlet.*;
+import org.springframework.boot.test.mock.mockito.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = StartWebApplication.class)
+public class GRITest {
+	@Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+	@Test
+	public void testHelloEndpointIsOK() throws Exception {
+        this.mockMvc.perform(get("/inicio"))
+            .andExpect(status().isOk());
+	}
+}
