616be3ffd03896ee7f3bebe9eeb85828619c8e9d
Jefferson <jarenas98@outlook.com>
2020-03-08 18:26:20 -0500

    migracion

--- src/main/java/co/edu/uniquindio/gri/dao/CentroDAO.java
+++ src/main/java/co/edu/uniquindio/gri/dao/CentroDAO.java
@@ -35,6 +35,7 @@ public class CentroDAO {
 	 * @return el centro especificado por el id
 	 */
 	public Centro getCentroById(Long centroId){
+		
 		return centroRepository.findById(centroId).orElse(null);
 	}
 	
